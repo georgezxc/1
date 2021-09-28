80a9b3e98e7f938ba1bfe6ad933117bb9086a792
georgezxc <george.github@bk.ru>
2021-09-28 21:10:13 +0300

    ПЗ 1

---
+++ ПЗ 1/Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a, b, c, rez;
+	cout << "enter a three-digit number: " << endl;
+	cin >> rez;
+	if (rez % 2 == 0)
+	{
+		cout << "it's an even number " << endl;
+	}
+	else
+	{
+		cout << "it's an odd number " << endl;
+	}
+
+	a = rez % 10;
+	b = rez / 10 % 10;
+	c = rez / 100;
+	cout << (a + b + c) / 3.0 << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ ПЗ 1/Задание 2/Задание 2/Задание 2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int month;
+	cout << "Enter the month number: ";
+	cin >> month;
+	switch (month)
+	{
+	case 12:
+	case 1:
+	case 2:
+		cout << "Winter" << endl;
+		break;
+	case 3:
+	case 4:
+	case 5:
+		cout << "Spring" << endl;
+		break;
+	case 6:
+	case 7:
+	case 8:
+		cout << "Summer" << endl;
+		break;
+	case 9:
+	case 10:
+	case 11:
+		cout << "Autumn" << endl;
+		break;
+	default:
+		cout << "this is not right" << endl;
+		break;
+	}
+	return 0;
+}
